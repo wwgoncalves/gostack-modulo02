9a643bb6b2ad51be2da340771f187091043fbe89
lilo <williamcco@gmail.com>
2019-08-31 22:34:11 -0300

    user data update under authorization

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,6 +23,39 @@ class UserController {
       return response.json({ error });
     }
   }
+
+  async update(request, response) {
+    try {
+      const { email, oldPassword } = request.body;
+
+      const user = await User.findByPk(request.userId);
+
+      if (email !== user.email) {
+        const userExists = await User.findOne({
+          where: { email }
+        });
+
+        if (userExists) {
+          return response.status(400).json({ error: "User already exists." });
+        }
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return response.status(401).json({ error: "Password does not match." });
+      }
+
+      const { id, name, provider } = await user.update(request.body);
+
+      return response.json({
+        id,
+        name,
+        email,
+        provider
+      });
+    } catch (error) {
+      return response.json({ error });
+    }
+  }
 }
 
 export default new UserController();
