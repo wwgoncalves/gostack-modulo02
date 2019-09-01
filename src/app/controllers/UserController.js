87c26758e75486a0c24d85e8d777a0f79b107885
lilo <williamcco@gmail.com>
2019-08-31 23:05:14 -0300

    data validation with Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,23 @@
+import * as Yup from "yup";
+
 import User from "../models/User";
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6)
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Validation fails" });
+    }
+
     try {
       const userExists = await User.findOne({
         where: { email: request.body.email }
@@ -25,11 +41,30 @@ class UserController {
   }
 
   async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      )
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: "Validation fails" });
+    }
+
     try {
       const { email, oldPassword } = request.body;
 
       const user = await User.findByPk(request.userId);
 
+      // in case user is updating their email
       if (email !== user.email) {
         const userExists = await User.findOne({
           where: { email }
@@ -40,6 +75,7 @@ class UserController {
         }
       }
 
+      // in case user is updating their password
       if (oldPassword && !(await user.checkPassword(oldPassword))) {
         return response.status(401).json({ error: "Password does not match." });
       }
